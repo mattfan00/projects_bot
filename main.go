33a466fd3898f3300f09b3c23c9b4bd8cbc11176
mattfan00 <kungfumo@gmail.com>
2020-09-24 16:41:20 -0400

    posts messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -16,11 +17,17 @@ type ConversationsList struct {
 	Channels []Channel `json:"channels"`
 }
 
+type PostMessage struct {
+	Token   string `json:"token"`
+	Text    string `json:"text"`
+	Channel string `json:"channel"`
+}
+
 func main() {
 	token := "xoxb-1392098059300-1385963107650-2tIYynqnoisg8oiJDHTNQ8HF"
-	url := fmt.Sprintf("https://slack.com/api/conversations.list?token=%s", token)
+	getUrl := fmt.Sprintf("https://slack.com/api/conversations.list?token=%s", token)
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(getUrl)
 	if err != nil {
 		panic(err)
 	}
@@ -36,6 +43,32 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(conversations.Channels[0])
+	channelId := conversations.Channels[0].Id
 
+	postUrl := "https://slack.com/api/chat.postMessage"
+	newMessage := PostMessage{token, "hello from go", channelId}
+	reqBody, err := json.Marshal(newMessage)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}
+	r, err := http.NewRequest("POST", postUrl, bytes.NewBuffer(reqBody))
+	if err != nil {
+		panic(err)
+	}
+	r.Header.Add("Content-Type", "application/json")
+	r.Header.Add("Authorization", "Bearer "+token)
+
+	res, err := client.Do(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(res.Body).Decode(&result)
+
+	fmt.Println(result)
 }
