75dd1c7e609af861ef0afdfd07fc35d73af949f4
mattfan00 <kungfumo@gmail.com>
2020-09-25 16:06:35 -0400

    bot resonds to mentions

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Channel struct {
@@ -17,14 +19,25 @@ type ConversationsList struct {
 	Channels []Channel `json:"channels"`
 }
 
-type PostMessage struct {
+type Message struct {
 	Token   string `json:"token"`
 	Text    string `json:"text"`
 	Channel string `json:"channel"`
 }
 
-func main() {
-	token := "xoxb-1392098059300-1385963107650-2tIYynqnoisg8oiJDHTNQ8HF"
+type SlackRequest struct {
+	Token     string `json:"token"`
+	Challenge string `json:"challenge"`
+	Type      string `json:"type"`
+	Event     struct {
+		Channel string `json:"channel"`
+		Text    string `json:"text"`
+	} `json:"event"`
+}
+
+var token = "xoxb-1392098059300-1385963107650-2tIYynqnoisg8oiJDHTNQ8HF"
+
+func GetChannelId() string {
 	getUrl := fmt.Sprintf("https://slack.com/api/conversations.list?token=%s", token)
 
 	resp, err := http.Get(getUrl)
@@ -43,10 +56,13 @@ func main() {
 		panic(err)
 	}
 
-	channelId := conversations.Channels[0].Id
+	return conversations.Channels[0].Id
 
+}
+
+func PostMessage(channelId string, message string) {
 	postUrl := "https://slack.com/api/chat.postMessage"
-	newMessage := PostMessage{token, "hello from go", channelId}
+	newMessage := Message{token, message, channelId}
 	reqBody, err := json.Marshal(newMessage)
 	if err != nil {
 		panic(err)
@@ -71,4 +87,53 @@ func main() {
 	json.NewDecoder(res.Body).Decode(&result)
 
 	fmt.Println(result)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello from the index")
+}
+
+func slackHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("reached the slack route")
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	// var newReq map[string]interface{}
+
+	var newReq SlackRequest
+
+	if err := json.Unmarshal(body, &newReq); err != nil {
+		panic(err)
+	}
+
+	/*
+		// fmt.Println(newReq["token"])
+		b, err := json.MarshalIndent(newReq, "", "  ")
+		fmt.Println(string(b))
+	*/
+
+	if newReq.Challenge != "" {
+		fmt.Printf("this is a challenge")
+		fmt.Fprintf(w, "%s", newReq.Challenge)
+	} else {
+		fmt.Printf("not a challenge")
+		fmt.Printf("%s", newReq.Event.Text)
+		PostMessage(newReq.Event.Channel, "hey dude")
+	}
+
+}
+
+func main() {
+	// PostMessage("no more messages")
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.Path("/slack").Methods("POST").HandlerFunc(slackHandler)
+	router.Path("/").Methods("GET").HandlerFunc(indexHandler)
+
+	http.ListenAndServe(":8080", router)
+
 }
